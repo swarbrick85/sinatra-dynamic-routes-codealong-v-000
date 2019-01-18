34b9a66d0cdddf917252aa54cb63222696d12200
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-18 17:20:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end 
   
-  get "/multiply?:num1/:num2" do 
+  get "/multiply/:num1/:num2" do 
     @num1 = params[:num1]
     @num2 = params[:num2]
     @num3 = @num1 * @num2 
